7f003e1de5475da5c28ac95cb0879faac4c0314d
zara ahmadi <zatokey@gmail.com>
2020-05-08 19:08:57 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,11 +3,11 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  return num2 - num1
+  return num1 - num2
 end
 
 def division(num1, num2)
-    return num2 / num1
+    return num1 / num2
 end
 
 def multiplication(num1, num2)
